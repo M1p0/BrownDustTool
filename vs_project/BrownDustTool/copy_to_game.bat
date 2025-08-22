59d67582fdd5383a9e7dea3a0e0b4a372c4bb32b
M1p0 <lx669196851@gmail.com>
2025-08-22 10:43:16 +0800

    Update copy_to_game.bat

--- vs_project/BrownDustTool/copy_to_game.bat
+++ vs_project/BrownDustTool/copy_to_game.bat
@@ -1,3 +1,3 @@
 copy ".\bin\Debug\netstandard2.1\BrownDustTool.dll" "..\..\..\Browndust2\Browndust2_10000001\BepInEx\plugins\BrownDustTool.dll"
-copy ".\bin\Debug\netstandard2.1\BrownDustTool.pdb" "..\..\..\\Browndust2\Browndust2_10000001\BepInEx\plugins\BrownDustTool.pdb"
+copy ".\bin\Debug\netstandard2.1\BrownDustTool.pdb" "..\..\..\Browndust2\Browndust2_10000001\BepInEx\plugins\BrownDustTool.pdb"
 Exit \b 0
