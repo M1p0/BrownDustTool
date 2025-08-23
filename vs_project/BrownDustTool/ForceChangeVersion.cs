6555f930c574d49735c6cd9e636b4dd2f670c6cc
M1p0 <lx669196851@gmail.com>
2025-08-23 15:32:54 +0800

    foce change game version

---
+++ vs_project/BrownDustTool/ForceChangeVersion.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using LobbyTableDumper;
+using Mosframe;
+using Proto.Net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static UnityEngine.ScriptingUtility;
+
+namespace BrownDustTool
+{
+    [HarmonyPatch(typeof(UnityEngine.Application), "version", MethodType.Getter)]
+    class ForceChangeVersion
+    {
+        static bool Prefix(ref string __result)
+        {
+            // 修改版本号为2.4.0
+            __result = "2.4.0";
+
+            // 返回false表示跳过原始方法调用
+            return false;
+        }
+    }
+}
